47f5d9cd318c7a1c4db4fab2ea47fdf2e0fb6174
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-05-03 21:18:09 +0700

    Pump Function Testing

---
+++ Testing/Relay/Relay.ino
@@ -0,0 +1,37 @@
+int relay = 7;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(relay, OUTPUT);
+  Serial.println("Enter how many ml you want to pump:");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    float ml = input.toFloat();
+    if (ml > 0) {
+      pumpMl(ml);
+      Serial.println("Pumping complete.");
+    } else {
+      Serial.println("Invalid input. Please enter a number.");
+    }
+  }
+}
+
+void pumpMl(float ml) {
+  float seconds = ml / 1.1111111; // flowrate (ml/s)
+  unsigned long milliseconds = (seconds * 1000) - 3000;
+
+  Serial.print("Pumping for ");
+  Serial.print(seconds);
+  Serial.println(" secs.");
+
+  if (milliseconds > 0) {
+  digitalWrite(relay, HIGH);
+  delay(milliseconds);
+  digitalWrite(relay, LOW);
+} else {
+  Serial.print("No way");
+}
+}
